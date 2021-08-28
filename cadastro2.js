85d3492dbdbcaec162588328b6893ce93e672e96
gabenasci <gabenascimento1@gmail.com>
2021-08-28 00:53:29 -0300

    Telas de cadastro apontam para a próxima tela; eventos agora com addEventListener()

--- cadastro2.js
+++ cadastro2.js
@@ -1,29 +1,74 @@
-function verificar_nome() {
-    if (inputs.nome.value == "") {
-        inputs.nome.style="border:1px solid;border-color: red"
+// Espera carregar a página
+document.addEventListener('DOMContentLoaded', function() {
+    
+    //Guarda os inputs em constantes
+    const cpf = document.querySelector('.cpf')
+    const estado = document.querySelector('.estado')
+    const cidade = document.querySelector('.cidade')
+
+    //Espera os eventos de blur ou click nos elementos selecionados
+    cpf.addEventListener("blur", verifyCPF)
+    estado.addEventListener("blur", verifyEstado)
+    cidade.addEventListener("blur", verifyCidade)
+
+})
+
+
+function verifyCPF() {
+    if (inputs.cpf.value == "") {
+        inputs.cpf.style="border:1px solid;border-color: red"
+        return false
+    }
+    else if (inputs.cpf.value.length !== 11) {
+        inputs.cpf.style="border:1px solid;border-color: red"
+        return false
+    }else{
+        inputs.cpf.style="border: 1px solid;border-color: grey"
+        return true
     }
 }
 
-function verificar_email() {
-    if (inputs.email.value == "") {
-        inputs.email.style="border:1px solid;border-color: red"
+function verifyEstado() {
+    if (inputs.estado.value == "") {
+        inputs.estado.style="border:1px solid;border-color: red"
+        return false
+    }else{
+        inputs.estado.style="border: 1px solid;border-color: grey"
+        return true
     }   
 }
 
-function verificar_senha1() {
-    if (inputs.senha1.value == "") {
-        inputs.senha1.style="border:1px solid;border-color: red"
+function verifyCidade() {
+    if (inputs.cidade.value == "") {
+        inputs.cidade.style="border:1px solid;border-color: red"
+        return false
+    }else{
+        inputs.cidade.style="border: 1px solid;border-color: grey"
+        return true
     }   
 }
 
-function verificar_senha2() {
-    if (inputs.senha2.value == "") {
-        inputs.senha2.style="border:1px solid;border-color: red"
-        window.alert("Digite a senha novamente")
-    } 
-    if (inputs.senha1.value !== inputs.senha2.value){
-        window.alert("As senhas não conferem")
-    }  
+function verifyCadastro1() {
+    verifyCPF()
+    if (inputs.cpf.value == "") {
+        window.alert("CPF obrigatório")
+    }
+    else if (inputs.cpf.value.length !== 11) {
+        window.alert("CPF inválido")
+    }
+    verifyEstado()
+    if (inputs.estado.value == "") {
+        window.alert("Estado obrigatório")
+    }
+    verifyCidade()
+    if (inputs.cidade.value == "") {
+        window.alert("Cidade obrigatória")
+    }
+
+    if (verifyCPF() && verifyEstado() && verifyCidade()) {
+        window.location = 'cadastro3.html'
+    }
+
+    return false
+    
 }
-
-
